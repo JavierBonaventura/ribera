221eb4f5ae7150d08c13030dd6557a1dc6605875
Javier Bonaventura <bonaventura.javier@gmail.com>
2023-06-13 22:39:24 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/home';
+import './App.css';
 
-const App = () => {
-  const [position1, setPosition1] = useState(0);
-  const [position2, setPosition2] = useState(0);
-  const [position3, setPosition3] = useState(0);
-  const [mostrarMenu1, setMostrarMenu1] = useState(false);
- 
-  const menu1 = false
-  
-  const opcion1 = () => {
-    setMostrarMenu1(true);
-    if (position2 === 0 && position3 === 0) {setPosition2(position2 + 10); setPosition3(position3 + 10)}
-    if (position2 === 10 && position3 === 10) {setPosition2(position2 - 10); setPosition3(position3 - 10)}
-    };
-
-  const opcion2 = () => {
-    if (position2 === 10 && position3 === 10) {setPosition2(position2 -10)}
-    if (position3 === 0 ) {setPosition3(position3 + 10); };
-    if (position2 === 0 && position3 === 10) {setPosition3(position3 - 10); }
-  };
-
-  const opcion3 = () => {
-    if (position3 == 10) {setPosition2(position3 - 10)};
-  };
-
+function App() {
   return (
-    <div className="container">
-      {/* <div
-        className="text"
-        style={{ transform: `translateY(${position}px)` }}
-      >
-        Texto
-      </div> */}
-      <div className="buttons">
-        <p className="text" style={{ transform: `translateY(${position1}px)` }} onClick={opcion1}>Opcion 1</p>
-        {mostrarMenu1  && <div>¡Este es el div que se mostrará!</div>}
-        <p className="text" style={{ transform: `translateY(${position2}px)` }} onClick={opcion2}>Opcion 2</p>
-        <p className="text" style={{ transform: `translateY(${position3}px)` }} onClick={opcion3}>Opcion 3</p>
-
-      </div>
-    </div>
+   <div>
+    <Home/>
+   </div>
   );
-};
+}
 
 export default App;
--- src/App_.js
+++ src/App_.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
-import '../App.css';
+import "./App.css";
 
-
-const Home = () => {
+const App = () => {
+  const [position1, setPosition1] = useState(0);
+  const [position2, setPosition2] = useState(0);
+  const [position3, setPosition3] = useState(0);
   const [mostrarMenu1, setMostrarMenu1] = useState(false);
   const [mostrarMenu2, setMostrarMenu2] = useState(false);
   const [mostrarMenu3, setMostrarMenu3] = useState(false);
 
 
+ 
+  const menu1 = false
   
   const opcion1 = () => {
     
@@ -15,6 +19,8 @@ const Home = () => {
     setMostrarMenu2(false);
     setMostrarMenu3(false);
     if (mostrarMenu1 === true) {setMostrarMenu1(false)}
+    // if (position2 === 0 && position3 === 0) {setPosition2(position2 + 10); setPosition3(position3 + 10)}
+    // if (position2 === 10 && position3 === 10) {setPosition2(position2 - 10); setPosition3(position3 - 10)}
     };
 
   const opcion2 = () => {
@@ -22,6 +28,9 @@ const Home = () => {
     setMostrarMenu2(true);
     setMostrarMenu3(false);
     if (mostrarMenu2 === true) {setMostrarMenu2(false)}
+    // if (position2 === 10 && position3 === 10) {setPosition2(position2 -10)}
+    // if (position3 === 0 ) {setPosition3(position3 + 10); };
+    // if (position2 === 0 && position3 === 10) {setPosition3(position3 - 10); }
   };
 
   const opcion3 = () => {
@@ -29,22 +38,29 @@ const Home = () => {
     setMostrarMenu2(false);
     setMostrarMenu3(true);
     if (mostrarMenu3 === true) {setMostrarMenu3(false)}
+    // if (position3 == 10) {setPosition2(position3 - 10)};
   };
 
   return (
     <div className="container">
+      {/* <div
+        className="text"
+        style={{ transform: `translateY(${position}px)` }}
+      >
+        Texto
+      </div> */}
       <div className="body">
       <div className="container">        
-        <p className="menu" onClick={opcion1}>Le Domaine</p>
+        <p className="menu" style={{ transform: `translateY(${position1}px)` }} onClick={opcion1}>Le Domaine</p>
         {mostrarMenu1  && <div className="subMenu">LE TERROIR</div>}
         {mostrarMenu1  && <div className="subMenu">L'HISTOIRE</div>}
         {mostrarMenu1  && <div className="subMenu">LES EQUIPES</div>}
-        <p className="menu"  onClick={opcion2}>Le Savoir-Faire</p>
+        <p className="menu" style={{ transform: `translateY(${position2}px)` }} onClick={opcion2}>Le Savoir-Faire</p>
         {mostrarMenu2  && <div className="subMenu">LE TRAVIAL DE LA VIGNE</div>}
         {mostrarMenu2  && <div className="subMenu">LE TRAVIAL DAS CHAIS</div>}
         {mostrarMenu2  && <div className="subMenu">LA CONSERVATION DE NOS VINS</div>}
         {mostrarMenu2  && <div className="subMenu">LE SERVICE & LA DEGUSTATION</div>}
-        <p className="menu"  onClick={opcion3}>Les Vins</p>
+        <p className="menu" style={{ transform: `translateY(${position3}px)` }} onClick={opcion3}>Les Vins</p>
         {mostrarMenu3  && <div className="subMenu">GRAND VIN</div>}
         {mostrarMenu3  && <div className="subMenu">PAVILLON ROUGE</div>}
         {mostrarMenu3  && <div className="subMenu">PAVILLON BLANC</div>}
@@ -66,4 +82,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default App;
