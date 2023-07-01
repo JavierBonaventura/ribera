d7f07492638d391273b1627e9393c1560ab6e6c9
Javier Bonaventura <bonaventura.javier@gmail.com>
2023-07-02 00:54:33 +0200

    modificas_01_julio

--- src/App.js
+++ src/App.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
-import Menu from './components/Menu'
-import History from './components/History';
-import AraucanaVineyard from './components/AraucanaVineyard'
-import WorkVineyard from './components/WorkVineyard'
-import WinesPreservation from './components/WinesPreservation'
-import WineYard from './components/WineYard'
-import RioCiervos from './components/RioCiervos'
-import Araucana from './components/Araucana'
-import AraucanaAzul from './components/AraucanaAzul'
-import Clasico from './components/Clasico'
-import Especial from './components/Especial'
-import ReservaEstibada from './components/ReservaEstibada'
-import MenuHamburguesa from './components/MenuHambuerguesa_'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Menu from "./components/Menu";
+import History from "./components/History";
+import AraucanaVineyard from "./components/AraucanaVineyard";
+import WorkVineyard from "./components/WorkVineyard";
+import WinesPreservation from "./components/WinesPreservation";
+import WineYard from "./components/WineYard";
+import RioCiervos from "./components/RioCiervos";
+import Araucana from "./components/Araucana";
+import AraucanaAzul from "./components/AraucanaAzul";
+import Clasico from "./components/Clasico";
+import Especial from "./components/Especial";
+import ReservaEstibada from "./components/ReservaEstibada";
+import MenuHamburguesa from "./components/MenuHambuerguesa_";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-   <div>   
-    <Routes>
-          <Route path="/" element={<Menu />} />   
-          <Route path="/history" element={<History />} />      
-          <Route path="/araucanavineyard" element={<AraucanaVineyard />} />   
-          <Route path="/workvineyard" element={<WorkVineyard />} />   
-          <Route path="/winespreservation" element={<WinesPreservation />} />             
-          <Route path="/wineyard" element={<WineYard />} />  
-          <Route path="/riociervos" element={<RioCiervos />} />  
+      <div>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/araucanavineyard" element={<AraucanaVineyard />} />
+          <Route path="/workvineyard" element={<WorkVineyard />} />
+          <Route path="/winespreservation" element={<WinesPreservation />} />
+          <Route path="/wineyard" element={<WineYard />} />
+          <Route path="/riociervos" element={<RioCiervos />} />
           <Route path="/araucana" element={<Araucana />} />
-          <Route path="/araucanaazul" element={<AraucanaAzul />} />  
-          <Route path="/clasico" element={<Clasico />} />  
-          <Route path="/especial" element={<Especial />} />  
-          <Route path="/reservaestibada" element={<ReservaEstibada />} />  
-          <Route path="/menuhamburguesa" element={<MenuHamburguesa />} />  
-          
-
-
+          <Route path="/araucanaazul" element={<AraucanaAzul />} />
+          <Route path="/clasico" element={<Clasico />} />
+          <Route path="/especial" element={<Especial />} />
+          <Route path="/reservaestibada" element={<ReservaEstibada />} />
+          <Route path="/menuhamburguesa" element={<MenuHamburguesa />} />
         </Routes>
-   </div>
-   </Router>
+      </div>
+    </Router>
   );
 }
 
--- src/components/MenuHambuerguesa.js
+++ src/components/MenuHambuerguesa.js
@@ -1,92 +1,156 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import '../App.css';
+import { Link } from "react-router-dom";
+import "../App.css";
 
 import logo from "../images/logo.svg";
-
-
-
-
 
 const Home = () => {
   const [mostrarMenu1, setMostrarMenu1] = useState(false);
   const [mostrarMenu2, setMostrarMenu2] = useState(false);
   const [mostrarMenu3, setMostrarMenu3] = useState(false);
 
-
-  
   const opcion1 = () => {
-    
     setMostrarMenu1(true);
     setMostrarMenu2(false);
     setMostrarMenu3(false);
-    if (mostrarMenu1 === true) {setMostrarMenu1(false)}
-    };
+    if (mostrarMenu1 === true) {
+      setMostrarMenu1(false);
+    }
+  };
 
   const opcion2 = () => {
     setMostrarMenu1(false);
     setMostrarMenu2(true);
     setMostrarMenu3(false);
-    if (mostrarMenu2 === true) {setMostrarMenu2(false)}
+    if (mostrarMenu2 === true) {
+      setMostrarMenu2(false);
+    }
   };
 
   const opcion3 = () => {
     setMostrarMenu1(false);
     setMostrarMenu2(false);
     setMostrarMenu3(true);
-    if (mostrarMenu3 === true) {setMostrarMenu3(false)}
+    if (mostrarMenu3 === true) {
+      setMostrarMenu3(false);
+    }
   };
 
   return (
     <div className="containerHome pt-8 backgroundHambuerguesa">
-      
       <div className="flex">
-       <img src={logo} alt="Imagen 1" className=" ml-36 w-1/4" />;
-       <Link to="/">
-<svg className="ml-16 pt-5" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
-  <circle cx="50" cy="50" r="16" stroke="#C3B17D" fill="transparent" strokeWidth="2" />
-  <line x1="44" y1="44" x2="56" y2="56" stroke="#C3B17D" strokeWidth="2" />
-  <line x1="44" y1="56" x2="56" y2="44" stroke="#C3B17D" strokeWidth="2" />
-</svg>
-</Link>
-</div>
-<div className="body">
-
-
-
-
-
-
-
-
-      <div className="containerHome pt-16">        
-        <p className="menu" onClick={opcion1}>Family</p>
-        {mostrarMenu1  &&  <Link to="/history" className="subMenu">HISTORY</Link>}
-        <p className="menu"  onClick={opcion2}>Patagonian Spirit</p>
-        {mostrarMenu2  && <Link to="/araucanavineyard" className="subMenu">ARAUCANA VINEYARD</Link>}
-        {mostrarMenu2  && <Link to="/workvineyard" className="subMenu">WORK IN THE VINEYARD</Link>}
-        {mostrarMenu2  && <Link to="/winespreservation" className="subMenu">WINES PRESERVATION</Link>}
-        {mostrarMenu2  && <Link to="/wineyard" className="subMenu">THE VINEYARD</Link>}
-        <p className="menu"  onClick={opcion3}>Wines</p>
-        {mostrarMenu3  && <Link to="/riociervos" className="subMenu">RÍO DE LOS CIERVOS</Link>}
-        {mostrarMenu3  && <Link to="/araucana" className="subMenu">ARAUCANA</Link>}
-        {mostrarMenu3  && <Link to="/araucanaazul" className="subMenu">ARAUCANA AZUL</Link>}
-        {mostrarMenu3  && <Link to="/clasico" className="subMenu">CLÁSICO</Link>}
-        {mostrarMenu3  && <Link to="/especial" className="subMenu">ESPECIAL</Link>}
-        {mostrarMenu3  && <Link to="/reservaestibada" className="subMenu">RESERVA ESTIBADA</Link>}
-
-        <div className="footer">
-          
-          <p className="footerItems">Contact</p>
-          <br></br>
-          <p className="footerItems">News</p>
-          <br></br>
-          <p className="footerItems">Casa Pirque</p>
-
-        </div>
-
+        <img src={logo} alt="Imagen 1" className=" ml-36 w-1/4" />;
+        <Link to="/">
+          <svg
+            className="ml-16 pt-5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="100"
+            height="100"
+            viewBox="0 0 100 100"
+          >
+            <circle
+              cx="50"
+              cy="50"
+              r="16"
+              stroke="#C3B17D"
+              fill="transparent"
+              strokeWidth="2"
+            />
+            <line
+              x1="44"
+              y1="44"
+              x2="56"
+              y2="56"
+              stroke="#C3B17D"
+              strokeWidth="2"
+            />
+            <line
+              x1="44"
+              y1="56"
+              x2="56"
+              y2="44"
+              stroke="#C3B17D"
+              strokeWidth="2"
+            />
+          </svg>
+        </Link>
       </div>
-    </div>
+      <div className="body">
+        <div className="containerHome pt-16">
+          <p className="menu" onClick={opcion1}>
+            Family
+          </p>
+          {mostrarMenu1 && (
+            <Link to="/history" className="subMenu">
+              HISTORY
+            </Link>
+          )}
+          <p className="menu" onClick={opcion2}>
+            Patagonian Spirit
+          </p>
+          {mostrarMenu2 && (
+            <Link to="/araucanavineyard" className="subMenu">
+              ARAUCANA VINEYARD
+            </Link>
+          )}
+          {mostrarMenu2 && (
+            <Link to="/workvineyard" className="subMenu">
+              WORK IN THE VINEYARD
+            </Link>
+          )}
+          {mostrarMenu2 && (
+            <Link to="/winespreservation" className="subMenu">
+              WINES PRESERVATION
+            </Link>
+          )}
+          {mostrarMenu2 && (
+            <Link to="/wineyard" className="subMenu">
+              THE VINEYARD
+            </Link>
+          )}
+          <p className="menu" onClick={opcion3}>
+            Wines
+          </p>
+          {mostrarMenu3 && (
+            <Link to="/riociervos" className="subMenu">
+              RÍO DE LOS CIERVOS
+            </Link>
+          )}
+          {mostrarMenu3 && (
+            <Link to="/araucana" className="subMenu">
+              ARAUCANA
+            </Link>
+          )}
+          {mostrarMenu3 && (
+            <Link to="/araucanaazul" className="subMenu">
+              ARAUCANA AZUL
+            </Link>
+          )}
+          {mostrarMenu3 && (
+            <Link to="/clasico" className="subMenu">
+              CLÁSICO
+            </Link>
+          )}
+          {mostrarMenu3 && (
+            <Link to="/especial" className="subMenu">
+              ESPECIAL
+            </Link>
+          )}
+          {mostrarMenu3 && (
+            <Link to="/reservaestibada" className="subMenu">
+              RESERVA ESTIBADA
+            </Link>
+          )}
+
+          <div className="footer">
+            <p className="footerItems">Contact</p>
+            <br></br>
+            <p className="footerItems">News</p>
+            <br></br>
+            <p className="footerItems">Casa Pirque</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
