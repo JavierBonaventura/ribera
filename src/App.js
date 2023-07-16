e11f5ba9ec2a75e3790da4da489c75b9b1f9b481
Javier Bonaventura <bonaventura.javier@gmail.com>
2023-07-16 18:53:19 +0200

    modificaciones_16_julio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react
 import { Transition, animated, config } from '@react-spring/web';
 
 import Menu from "./components/Menu";
+import MenuDesktop from "./components/MenuDesktop";
 import Family from "./components/Family";
 import AraucanaVineyard from "./components/AraucanaVineyard";
 import WorkVineyard from "./components/WorkVineyard";
@@ -16,6 +17,7 @@ import Clasico from "./components/Clasico";
 import Especial from "./components/Especial";
 import ReservaEstibada from "./components/ReservaEstibada";
 import MenuHamburguesa from "./components/MenuHamburguesa";
+import MenuHamburguesaDesktop from './components/MenuHamburguesaDesktop';
 import "./App.css";
 
 import BlueValley from "./components/BlueValley";
@@ -36,6 +38,20 @@ function App() {
 const AppContent = () => {
   const location = useLocation();
   const [showBlackScreen, setShowBlackScreen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     // Mostrar la pantalla negra durante 500ms al cambiar de página
@@ -74,7 +90,11 @@ const AppContent = () => {
               />
             )}
             <Routes location={item}>
-            <Route path="/" element={<Menu />} />
+            {windowWidth < 768 ? (
+                <Route path="/" element={<Menu />} />
+              ) : (
+                <Route path="/" element={<MenuDesktop />} />
+              )}
           <Route path="/family" element={<Family />} />
           <Route path="/bluevalley" element={<BlueValley />} />
           <Route path="/araucanavineyard" element={<AraucanaVineyard />} />
@@ -88,7 +108,11 @@ const AppContent = () => {
           <Route path="/clasico" element={<Clasico />} />
           <Route path="/especial" element={<Especial />} />
           <Route path="/reservaestibada" element={<ReservaEstibada />} />
-          <Route path="/menuhamburguesa" element={<MenuHamburguesa />} />
+          {windowWidth < 768 ? (
+                <Route path="/menuhamburguesa" element={<MenuHamburguesa />} />
+              ) : (
+                <Route path="/menuhamburguesa" element={<MenuHamburguesaDesktop />} />
+              )}
           <Route path="/contacto" element={<FormularioContacto />} />
             </Routes>
           </animated.div>
---
+++ src/components/MenuHamburguesaDesktop.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import fondoHamburguesaDesktop from '../images/fondoHamburguesaDesktop.png';
+import logoDesktop from '../images/logoDesktop.png';
+import hambur from "../images/menu-hambur.png";
+
+
+
+const MenuHamburguesaDesktop = () => {
+  const [isMenu1Hovered, setMenu1Hovered] = useState(false);
+  const playfairFontRegular = {
+    fontFamily: "Playfair Regular, sans-serif",
+    fontWeight: "normal",
+    fontStyle: "normal",
+  };
+
+  const handleMenu1Hover = () => {
+    console.log("test")
+    setMenu1Hovered(true);
+  };
+
+  const handleMenu1Leave = () => {
+    setMenu1Hovered(false);
+  };
+
+  return (
+    <div
+      className={`h-screen bg-[imagen] bg-no-repeat bg-cover bg-center relative`}
+      style={{ backgroundImage: `url(${fondoHamburguesaDesktop})` }}
+    >
+      <div className="absolute inset-0 mx-6 my-6 border border-white opacity-60"></div>
+      <div className="absolute inset-0 mx-8 my-8 border border-white opacity-40"></div>
+      <div className="fixed top-10 left-1/2 -ml-20 w-36">
+  <img
+    src={logoDesktop}
+    alt="Logo"
+    className=""
+  />
+</div>
+<img
+        src={hambur}
+        alt="Hamburger"
+        className="fixed top-16 right-16 w-6 h-6 cursor-pointer"
+      />
+
+      <div className="flex justify-center items-center h-full">
+        <div className="w-1/5"></div>
+        <div id='menu1'  onMouseEnter={handleMenu1Hover}  onMouseLeave={handleMenu1Leave}  className=" w-1/5 flex flex-col items-center"> {/* Alinear los elementos en una columna */}
+          <p style={playfairFontRegular} className="text-center menu sombra-menu tracking-wider">History</p>
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest ">FAMILY</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">VALLE AZUL PATAGONIA</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest opacity-0">SubMenu</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest opacity-0">SubMenu</p> 
+
+        </div>
+       
+        <div id='menu2'  className={`w-1/3 flex flex-col items-center ${isMenu1Hovered ? 'opacity-50' : 'opacity-100'}`}> {/* Alinear los elementos en una columna */}
+          <p style={playfairFontRegular} className="text-center menu sombra-menu tracking-wider">Patagonian Spirit</p>
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">ARAUCANA VINEYARD</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">WORK IN THE VINEYARD</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">WINES PRESERVATION</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">LIFE IN THE VINEYARD</p> 
+        </div>
+        <div id='menu3' className="w-1/5 flex flex-col items-center"> {/* Alinear los elementos en una columna */}
+          <p style={playfairFontRegular} className="text-center menu sombra-menu tracking-wider">Wines</p>
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">IN ARGENTINA</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest">IN THE WORLD</p>
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest opacity-0">SubMenu</p> 
+          <p style={playfairFontRegular} className="text-center subMenu tracking-widest opacity-0">SubMenu</p> 
+ 
+        </div>
+        <div className="w-1/5"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuHamburguesaDesktop;
