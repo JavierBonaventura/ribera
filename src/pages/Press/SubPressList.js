cf4a206ce735db7c7a104e616f877c8f5777364e
Javier Bonaventura <bonaventura.javier@gmail.com>
2024-01-08 20:50:30 +0100

    modificas 8 de enero

--- src/pages/Press/SubPressList.js
+++ src/pages/Press/SubPressList.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const SubPressList = ({ year, presses }) => {
-  console.log("en ano es " + year)
-  console.log(presses)
+  // console.log("en ano es " + year)
+  // console.log(presses)
   const { t, i18n } = useTranslation();
 
   const [isVisible, setIsVisible] = useState(false);
@@ -17,6 +17,18 @@ const SubPressList = ({ year, presses }) => {
 
     return () => clearTimeout(timeout);
   }, [year]);
+
+
+// Filter presses for the year 2024
+const filteredPresses = presses.filter((press) => {
+  const pressYear = new Date(press.attributes.date).getFullYear();
+  return pressYear === year;
+});
+
+console.log("el nuevo arreglo es", filteredPresses);
+
+
+
 
   const playfairFontItalic = {
     fontFamily: "Playfair Italic, serif",
@@ -38,7 +50,7 @@ const SubPressList = ({ year, presses }) => {
           : "opacity-0 transition-opacity duration-500"
       }`}
     >
-      {presses.map((press) => (
+      {filteredPresses.map((press) => (
         <div key={press.id} className="w-full">
           <div className="flex flex-col lg:flex-row lg:gap-x-10 w-full xl:w-2/3 mx-auto">
             <div
